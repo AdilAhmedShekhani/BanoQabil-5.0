4349c6c8409643eeca9c7544987aba68f84902fa
AdilAhmedShekhani <adilaziz2009@live.com>
2026-05-20 22:18:01 +0500

    Data Types

--- Webdevelopment with AI/Javascript/Class 01/app.js
+++ Webdevelopment with AI/Javascript/Class 01/app.js
@@ -1,1 +1,31 @@
+// Alert
+
+alert("Hello World");
+
+
+// Console
 console.log("Hello World");
+
+// Data Types
+
+// String
+var name = "Adil Ahmed";
+console.log(name);
+
+// Number
+var age = 25;
+console.log(age);
+
+// Boolean
+var isStudent = true;
+console.log(isStudent);
+
+// Undefined
+var address;
+console.log(address);
+
+// Null
+var city = null;
+console.log(city);
+
+
