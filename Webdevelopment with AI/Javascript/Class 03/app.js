79d307507b906d43d294b595fd4f172763ac7db8
AdilAhmedShekhani <adilaziz2009@live.com>
2026-06-09 22:37:57 +0500

    Arrow Function

--- Webdevelopment with AI/Javascript/Class 03/app.js
+++ Webdevelopment with AI/Javascript/Class 03/app.js
@@ -1,1 +1,24 @@
 console.log("Hello")
+
+// Create Function
+
+// Normal Function
+
+// function abc() {
+//   var abc = "name";
+//   console.log("abc==>", abc);
+// }
+
+// abc() // Function Call
+
+// Create Function
+
+// Arrow Function (ES-6)
+
+// const abc = () => {
+//   var abc = "name";
+//   console.log("abc==>", abc);
+// };
+
+// abc(); // Function Call
+
